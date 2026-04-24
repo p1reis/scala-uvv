6469f201ef2af8423942add4bc5f4b30013387e5
gabrieldenti <gabrieldentii@outlook.com.br>
2026-04-24 18:10:35 -0300

    feat: integrate swagger with custom port and controller decorators

--- backend/src/infrastructure/auth/auth.controller.ts
+++ backend/src/infrastructure/auth/auth.controller.ts
@@ -1,13 +1,22 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
 import { AuthService } from "../../application/auth/auth.service";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { Login } from "../../application/auth/login.dto";
 
+@ApiTags("Autenticação")
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @HttpCode(HttpStatus.OK)
   @Post("login")
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ 
+    summary: "Autenticar usuário", 
+    description: "Valida as credenciais dos usuários." 
+  })
+  @ApiResponse({ status: 200, description: "Login realizado com sucesso." })
+  @ApiResponse({ status: 400, description: "Erro de validação." })
+  @ApiResponse({ status: 401, description: "Credenciais incorretas." })
   async login(@Body() login: Login) {
     return this.authService.login(login);
   }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,13 @@
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
-import { ZodValidationPipe } from "nestjs-zod";
-// import { patchNestJsSwagger } from "nestjs-zod";
+import { ZodValidationPipe, patchNestJsSwagger } from "nestjs-zod";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
+
+  patchNestJsSwagger();
+
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
@@ -20,6 +23,24 @@ async function bootstrap() {
   });
 
   await app.listen(port);
+
   console.log(`🚀 Application is running on: http://localhost:${port}`);
+
+  const swaggerApp = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Scala UVV - API')
+    .setDescription('Serviço de agendamento de espaços UVV')
+    .setVersion('0.0.1')
+    .addBearerAuth()
+    .build();
+    
+  const document = SwaggerModule.createDocument(swaggerApp, config);
+  SwaggerModule.setup('/', swaggerApp, document);
+
+  const swaggerPort = configService.get<number>("SWAGGER_PORT") || 4000;
+
+  await swaggerApp.listen(swaggerPort);
+  console.log(`📚 Swagger http://localhost:${swaggerPort}`);
 }
 bootstrap();
